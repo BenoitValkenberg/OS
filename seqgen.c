6432f4e57b51449686cba220a0fb468485d45ff6
Benoit Valkenberg <benoitvalkenberg@hotmail.com>
2015-02-22 14:39:07 +0100

    Première version

--- seqgen.c
+++ seqgen.c
@@ -1,12 +1,25 @@
 #include <linux/linkage.h>
 #include <asm/uaccess.h>
+#include <linux/slab.h>
 
 asmlinkage long sys_seqgen(unsigned char* ptr, size_t size){
 	
-	if(true)
-		return 0;
-	else{
-		printk("SEQGEN error ...\n");
+	if(ptr == NULL || size > 0){
+		printk("SEQGEN Wrong arguments\n");
 		return -1;
 	}
+	
+	char* seq = kmalloc(size, GFP_KERNEL);
+	
+	if(seq == NULL){
+		printk("SEQGEN Allocation error\n");
+		return -1;
+	}
+			
+	copy_from_user(seq, ptr, strlen_user(ptr));
+	
+	printk("mysyscall: %s\n", seq);
+	
+	
+	return 0;
 }
