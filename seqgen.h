0484c02d66eb43843592256bfd20d59d26a05fca
Celh <quentin.boileau@hotmail.be>
2015-03-01 20:17:38 +0100

    Update seqgen.h

--- seqgen.h
+++ seqgen.h
@@ -1,11 +1,13 @@
 #ifndef SEQGEN_H_
 #define SEQGEN_H_
 
-typedef struct Sequence_t Sequence;
+typedef struct Sequence_t* Sequence;
 struct Sequence_t{
 	size_t size;
 	unsigned char* seq;
 };
+
+extern Sequence sequence;
 
 void freeSequence(void);
 
